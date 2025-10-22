4e091bd3e51a91a0f2543a1a95c0b99ae1913d45
serjimen <serjimen@student.42madrid.com>
2025-10-22 11:44:26 +0200

    issue
    Se arregla el issue de las pruebas del expander

--- src/expander/expander_utils2.c
+++ src/expander/expander_utils2.c
@@ -6,7 +6,7 @@
 /*   By: serjimen <serjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 10:53:48 by serjimen          #+#    #+#             */
-/*   Updated: 2025/10/22 11:32:29 by serjimen         ###   ########.fr       */
+/*   Updated: 2025/10/22 11:41:11 by serjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,9 @@ char	*append_literal(t_arg *arg, size_t i)
 	char	*new;
 
 	old = arg->exp_value;
-	new = ft_calloc(3, sizeof(char));
+	new = ft_calloc(2, sizeof(char));
 	new[0] = '$';
-	new[1] = '"';
-	new[2] = '\0';
+	new[1] = '\0';
 	arg->exp_value = ft_strjoin(old, new);
 	free(old);
 	free(new);
